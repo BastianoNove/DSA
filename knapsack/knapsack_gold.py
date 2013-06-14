e7bf813774d6b3c118e127faadb3b11a1d79f623
Sebastian <sebastian.ramirez@intel.com>
2013-06-14 13:52:34 -0700

    Knapsack gold tests

--- knapsack/knapsack_gold.py
+++ knapsack/knapsack_gold.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python
 
 def knapsack(w, c):
+    w.sort()
     w = [0] + w
     dp = [[0 for _ in xrange(c+1)] for _ in xrange(len(w))]
     dp[0][0] = 1
-    print_table(dp)
     for i in range(1, len(w)):
         for j in range(1, c+1):
             if (w[i] > j):
-                dp[i][j] = 0 
+                dp[i][j] = 0
             else:
-                print(j-w[i])
-                dp[i][j] = int(dp[i-1][j-w[i]] == 1)
-    print_table(dp)
-    return dp[len(w)-1][c]
+                dp[i][j] = int(dp[i-1][j-w[i]] == 1 or (j-w[i] == 0))
+    return max(dp[i][c] for i in range(1,len(w)))
 
+def backtrack(dp, c, w):
+    output = []
+    k = len(w)
+    while c:
+        for i in range(k-1, 1, -1):
+            if dp[i][c]:
+                output[0:0] = [i]
+                c = c - w[i]
+                break
+        c = 0 #not possible
+    return output
 
 def print_table(dp):
     for row in dp:
@@ -22,7 +31,10 @@ def print_table(dp):
     print
 
 def test():
-    print knapsack([1,2,3], 5)
+    assert(knapsack([3,2,1], 7) == 0)
+    assert(knapsack([3,2,1], 5) == 1)
+    assert(knapsack([1,11,2,89], 13) == 1)
+    print 'test pass'
 
 test()
 
