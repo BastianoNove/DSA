665696e61a3d1e2689b9d74100dd0e43087a6904
Sebastian <sebas018000@gmail.com>
2013-06-21 00:15:38 -0700

    Implement bellman ford

---
+++ graph/bellman_ford.py
@@ -0,0 +1,21 @@
+import graph
+
+def bellman_ford(g, w, s):
+    graph.init_single_source(g, s) 
+    for i in range(1, len(g.keys())):
+        for u,v in graph.edges:
+            graph.relax(graph.vertices[u], graph.vertices[v], w)
+    for u,v in graph.edges:
+        if graph.vertices[v].d > graph.vertices[u].d + w[(u,v)]
+            return False
+    return True
+
+def test():
+    w = { (1,2) : 7, (1,3) : 9, (1,6) : 14, (2,4) : 15,
+             (2,3) : 10, (6,3) : 2, (6,5) : 9, (3, 4) : 11,
+             (5,4) : 6 }
+    w = graph.weights(w) 
+    g = graph.make_graph(w.keys())
+    print 'tests pass'
+
+test()
