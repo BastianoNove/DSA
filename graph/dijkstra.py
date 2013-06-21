83fc77a66d8197fcc2a53a1e239b300eb65a3425
Sebastian <sebas018000@gmail.com>
2013-06-20 22:01:56 -0700

    reuse weights

--- graph/dijkstra.py
+++ graph/dijkstra.py
@@ -6,12 +6,10 @@ def dijkstra(graph, costs):
     pass
 
 def test():
-    _cost = { (1,2) : 7, (1,3) : 9, (1,6) : 14, (2,4) : 15,
+    cost = { (1,2) : 7, (1,3) : 9, (1,6) : 14, (2,4) : 15,
              (2,3) : 10, (6,3) : 2, (6,5) : 9, (3, 4) : 11,
              (5,4) : 6 }
-    cost = defaultdict(lambda : float('inf'))
-    for key, value in _cost.items():
-        cost[key] = value
+    cost = graph.weights(cost.keys())
     g = graph.make_graph(cost.keys())
     #dijkstra(g, cost)
 
