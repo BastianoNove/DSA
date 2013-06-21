22465c33e33af690a7220db792c1bfee170a099e
Sebastian <sebas018000@gmail.com>
2013-06-20 22:20:45 -0700

    Add path estimate and predecessor attributes

--- graph/node.py
+++ graph/node.py
@@ -1,4 +1,6 @@
 class Node():
     def __init__(self, key):
         self.key = key 
-        self.edges = [] 
+        self.edges = []
+        self.d = float('inf') #shortest path estimate
+        self.predecesor = None
