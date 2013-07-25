0376421c70f99ce4ed07e987342a999a6790ac9f
Sebastian <sebas018000@gmail.com>
2013-07-24 22:22:36 -0700

    Add heap

---
+++ heap/heap.py
@@ -0,0 +1,50 @@
+import random
+
+class Heap(object):
+    def __init__(self, a=None):
+        self.A = []
+        if a is not None:
+            self.A = a 
+            self.build()
+
+    def size(self):
+        return len(self.A) - 1
+
+    def heapify(self, i):
+        assert(i>=0)
+        left = i * 2 + 1
+        right = i * 2 + 2 
+        if left <= self.size() and self.A[left] > self.A[i]:
+            largest = left 
+        else:
+            largest = i
+        if right <= self.size() and self.A[right] > self.A[largest]:
+            largest = right
+        if largest != i:
+            self.A[i], self.A[largest] = self.A[largest], self.A[i]
+            self.heapify(largest)
+
+    def build(self):
+        for i in range(len(self.A)//2, 0, -1):
+            self.heapify(i-1)
+
+    def insert(self, x):
+        self.A.append(x)
+        self.heapify(self.size())
+
+def check_heap(heap):
+    for i in range(len(heap.A)//2):
+        left = i * 2 + 1
+        right = i * 2 + 2
+        if left < heap.size():
+            assert(heap.A[i] >= heap.A[left])
+        if right < heap.size():
+            assert(heap.A[i] >= heap.A[right])
+
+def test():
+    for i in range(random.randint(1,100)*100):
+        heap = Heap([int(random.random()*100) for _ in range(random.randint(1, 100))])
+        check_heap(heap)
+    print('tests pass')
+
+test()
