37146c71f8437a59dffc7271dbadfb3605286022
= <sebas018000@gmail.com>
2013-07-25 11:50:28 -0700

    Fix heapsort. Add tests

--- heap/heap_sort.py
+++ heap/heap_sort.py
@@ -4,14 +4,18 @@ import random
 def heap_sort(A):
     heap = Heap(A)
     while heap.size():
-        heap.A[heap.size()], heap.A[1] = heap.A[1], heap.A[heap.size()]
+        heap.A[heap.size()], heap.A[0] = heap.A[0], heap.A[heap.size()]
         heap._size -= 1
         heap.heapify(0)
     return heap.A
 
 def test():
-    sequence = [random.randint(1,100) for _ in range(5)]
-    print(sequence)
-    sequence = heap_sort(sequence)
-    print(sequence)
-test()
+    for i in range(1000):
+        sequence = [random.randint(1,100) for _ in range(random.randint(1,100))]
+        sorted_test = sorted(sequence)
+        heap = heap_sort(sequence)
+        assert(all(heap[i] == sorted_test[i] for i in range(len(sorted_test))))
+    print('tests pass')
+
+if __name__ == '__main__':
+    test()
