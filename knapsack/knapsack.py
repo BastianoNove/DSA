b25f84a1d99c8e504ded90e5fd79aa680f6b9c8f
Sebastian <sebas018000@gmail.com>
2013-07-30 22:48:32 -0700

    knapsack to py3. Execute if main program

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -3,7 +3,7 @@
 def knapsack(w, v, c):
     w = [0] + w
     v = [0] + v
-    dp = [[0 for _ in xrange(c+1)] for _ in xrange(len(w))]
+    dp = [[0 for _ in range(c+1)] for _ in range(len(w))]
     for i in range(1, len(w)):
         for j in range(1, c+1):
             if (w[i] > j):
@@ -15,13 +15,14 @@ def knapsack(w, v, c):
 
 def print_table(dp):
     for row in dp:
-        print row
+        print(row)
     print
 
 def test():
     assert(knapsack([1,2,3], [1,2,3], 5) == 5)
     assert(knapsack([1, 5, 3, 4], [15,10, 9, 5], 8) == 29)
     assert(knapsack([12, 2, 1, 4, 1], [4, 2, 1, 10, 2], 15) == 15)
-    print 'test pass'
+    print('test pass')
 
-test()
+if __name__ == '__main__':
+    test()
