163d5944f6dec2b171bdacc5667ae132eb6691c4
= <sebas018000@gmail.com>
2013-08-02 12:35:07 -0700

    Rename function to counting_sort

--- sort/counting.py
+++ sort/counting.py
@@ -1,7 +1,7 @@
 import random
 
-def counting(items, k, key=lambda x: x):
-    L = [[] for i in range(k+1)] 
+def counting_sort(items, k, key=lambda x: x):
+    L = [[] for i in range(k+1)]
     for j in range(len(items)):
         L[key(items[j])].append(items[j])
     output = []
@@ -13,7 +13,7 @@ def test():
     for _ in range(1000):
         a = [random.randint(0,100) for i in range(100)]
         b = sorted(a)
-        a = counting(a, max(a))
+        a = counting_sort(a, max(a))
         assert(all(a[i] == b[i] for i in range(len(a))))
     print('tests pass')
 
