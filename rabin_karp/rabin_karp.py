501dda7da6955bbb1407f04fbcc53d788741108c
Sebastian <sebas018000@gmail.com>
2013-08-06 08:28:22 -0700

    Add tests (ugly code.) Sync commit

--- rabin_karp/rabin_karp.py
+++ rabin_karp/rabin_karp.py
@@ -8,12 +8,44 @@ class RabinKarp(object):
         self.inverse_base = pow(self.base, self.p-1, self.p) % self.p
 
     def append(self, x):
-        self.hash_value= (self.hash_value * self.base + new) % p
-        self.base_to_size = (self.base_to_size * base) % p
+        self.hash_value= (self.hash_value * self.base + x) % self.p
+        self.base_to_size = (self.base_to_size * self.base) % self.p
 
-    def skip(self):
-        self.hash_value = (self.hash_value - old * magic + p * base ) % p
-        self.base_to_size = self.base_to_size * self.inverse_base % p
+    def skip(self, old):
+        self.hash_value = (self.hash_value - old * self.base_to_size + self.p * self.base ) % self.p
+        self.base_to_size = (self.base_to_size * self.inverse_base) % self.p
 
     def hash_value(self):
         return self.hash_value
+
+
+def test():
+    doc = 'abcdefgh'
+    to_find = 'bc'
+    h_find = RabinKarp(256, 23)
+    h_doc = RabinKarp(256, 23) 
+    for letter in to_find:
+        print('appending: ', letter)
+        h_find.append(ord(letter))
+    print('bulding h_doct')
+    for letter in doc[:len(to_find)]:
+        print('appending : ', letter)
+        h_doc.append(ord(letter))
+    
+    old = 0
+    for i in range(len(to_find), len(doc)):
+        print('hf: ', h_find.hash_value, 'hdoc: ', h_doc.hash_value)
+        if h_find.hash_value == h_doc.hash_value:
+            print('found match')
+        h_doc.append(ord(doc[i]))
+        h_doc.skip(ord(doc[old]))
+        print('appending: ', doc[i])
+        print('skipping: ', doc[old], old)
+        print('hf: ', h_find.hash_value, 'hdoc: ', h_doc.hash_value)
+        old += 1
+    else:
+        print("no match")
+
+if __name__ == '__main__':
+    test()
+    
