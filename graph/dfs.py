8bba833d8f42cb13bdf6beb0821c25bdcbf7cdb2
Sebastian <sebas018000@gmail.com>
2013-09-09 23:18:57 -0700

    Visit node, not just key

--- graph/dfs.py
+++ graph/dfs.py
@@ -12,7 +12,7 @@ def dfs(node, visited=None, visit=None):
     for v in node.edges:
         if v not in visited:
           dfs(v, visited, visit)
-    visit(node.key)
+    visit(node)
 
 if __name__ == '__main__':
     g = graph.make_graph([('y','z'), ('a','b'), ('a', 'd'),  ('b', 'c'), ('c', 'd'), ('b', 'y'), ('c', 'a'), ('d', 'y'), ])
