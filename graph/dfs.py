5d1ec0f388a3e074547f77764af762c202ca12ba
Sebastian <sebas018000@gmail.com>
2013-09-09 22:30:29 -0700

    Add visit function to dfs

--- graph/dfs.py
+++ graph/dfs.py
@@ -1,14 +1,16 @@
 from node import Node
 import graph
 
-def dfs(node, visited=[]):
+def dfs(node, visited=[], visit=None):
+    if visit is None:
+        visit = print
     if node in visited:
         return
     visited.append(node)
     for v in node.edges:
         if v not in visited:
-          dfs(v, visited)
-    print(node.key)
+          dfs(v, visited, visit)
+    visit(node.key)
 
 g = graph.make_graph([('y','z'), ('a','b'), ('a', 'd'),  ('b', 'c'), ('c', 'd'), ('b', 'y'), ('c', 'a'), ('d', 'y'), ])
 dfs(g.vertices['a'])
