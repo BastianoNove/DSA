50e10a2fdfc26024abb9bcba4c167aaac044943a
Sebastian <sebas018000@gmail.com>
2013-09-15 23:40:11 -0700

    Prevent infinte cycle when printing nodes

--- graph/node.py
+++ graph/node.py
@@ -5,5 +5,6 @@ class Node():
         self.d = float('inf') #shortest path estimate
         self.predecessor = None
     def __repr__(self):
+        predecessor = self.predecessor.key if self.predecessor else None
         return 'Node {0}, d {1}, predecessor {2}, edges {3}'.format(str(self.key),
-          str(self.d), str(self.predecessor), str([x.key for x in self.edges]))
+          str(self.d), str(predecessor), str([x.key for x in self.edges]))
