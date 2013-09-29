cb68232ddc0ad9b4491c202f9052a79c07532c84
Sebastian <sebas018000@gmail.com>
2013-09-28 23:45:21 -0700

    Implement __lt__ in node

--- graph/node.py
+++ graph/node.py
@@ -4,7 +4,12 @@ class Node():
         self.edges = []
         self.d = float('inf') #shortest path estimate
         self.predecessor = None
+
     def __repr__(self):
         predecessor = self.predecessor.key if self.predecessor else None
         return 'Node {0}, d {1}, predecessor {2}, edges {3}'.format(str(self.key),
           str(self.d), str(predecessor), str([x.key for x in self.edges]))
+
+    def __lt__(self, other):
+        return self.d < other.d
+
