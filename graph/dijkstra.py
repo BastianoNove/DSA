881eaa53ec74b25cef6309cd0ae88fdcd174bbe3
Sebastian <sebas018000@gmail.com>
2013-09-29 17:24:42 -0700

    Refactor test graph. Add CLRS testcase.

--- graph/dijkstra.py
+++ graph/dijkstra.py
@@ -29,15 +29,26 @@ def dijkstra(graph, weights, start_vertex):
         for v in u.edges:
             graph_utils.relax(u, v, weights)
 
-def test():
-    cost = { (1,2) : 7, (1,3) : 9, (1,6) : 14, (2,4) : 15,
-             (2,3) : 10, (6,3) : 2, (6,5) : 9, (3, 4) : 11,
-             (5,4) : 6 }
+def test_graph(cost, source):
     cost = graph_utils.weights(cost)
     g = graph_utils.make_graph(cost.keys())
-    dijkstra(g, cost, g.vertices[1])
+    dijkstra(g, cost, g.vertices[source])
     for vertex in g.vertices.values():
         print(vertex)
+
+def test():
+    test_one = { (1,2) : 7, (1,3) : 9, (1,6) : 14, (2,4) : 15,
+             (2,3) : 10, (6,3) : 2, (6,5) : 9, (3, 4) : 11,
+             (5,4) : 6 }
+    print('Test one: ')
+    test_graph(test_one, 1)
+
+    test_two = { ('s', 't') : 10, ('t', 'x') : 1, ('s', 'y') : 5,
+            ('y', 't') : 3, ('t', 'y') : 2, ('y', 'x') : 9,
+            ('y', 'z') : 2, ('z', 's') : 7, ('z', 'x') : 6,
+            ('x', 'z') : 4}
+    print('Test two: ')
+    test_graph(test_two, 's')
 
 if __name__ == '__main__':
     test()
