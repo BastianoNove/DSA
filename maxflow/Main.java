7e150260f537a80ddb8147e10bb283442b15dcd2
Sebastian <sebas018000@gmail.com>
2013-12-11 20:44:32 -0800

    Add Main file.

---
+++ maxflow/Main.java
@@ -0,0 +1,24 @@
+class Main {
+  public static void main(String args[]) {
+    Graph g = new Graph(10);
+    g.addEdge(1, 2);
+    g.addEdge(2, 3);
+    g.addEdge(2, 5);
+    g.addEdge(2, 4);
+    g.addEdge(3, 6);
+    g.addEdge(1, 7);
+    g.addEdge(5, 7);
+    g.addEdge(6, 8);
+    g.addEdge(3, 8);
+    g.addEdge(4, 9);
+    g.addEdge(8, 9);
+
+    Bfs iter = new Bfs(g, g.vertices.get(1));
+    Vertex current;
+    while (iter.hasNext()) {
+      current = iter.next();
+      System.out.printf("Visiting vertex %d.\n", current.key);
+    }
+  }
+}
+ 
